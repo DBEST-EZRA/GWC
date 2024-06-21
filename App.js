e7c8707583737658b8cc05e6abc9f92c299b74b1
DBEST-EZRA <ezradbest101@gmail.com>
2024-06-21 12:29:51 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View>
+      <Text>This is my awesome app</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
+// ConcreteHub
